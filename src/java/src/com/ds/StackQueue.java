bc261f69652bd14e2b873bb7e3fe691658b1b2ac
swaminathan.ms <swaminathan.ms@zohocorp.com>
2016-03-18 13:20:49 +0530

    Stack as queue implementation

--- src/java/src/com/ds/StackQueue.java
+++ src/java/src/com/ds/StackQueue.java
@@ -25,6 +25,20 @@ public class StackQueue {
 			System.out.printf("Min (%s): %d\n", qm.displayQueue(), qm.getMinElement());
 			qm.remove();
 		}
+
+		System.out.println("Stack as Queue implementation:");
+		StackAsQueue saq = new StackAsQueue();
+		for(int i: arr) {
+			saq.insert(i);
+			if(i%2 == 0) {
+				saq.removeRecursive();
+            			//saq.remove();
+			}
+		}
+
+		while(!saq.isEmpty()) {
+			System.out.println("Removed: " + saq.removeRecursive());
+		}
 		
 	}
 
@@ -115,4 +129,48 @@ class QueueMin {
 	public boolean isEmpty() {
 		return mainQueue.isEmpty();
 	}
+}
+
+class StackAsQueue {
+	
+	Stack<Integer> stack = new Stack<>();
+	Stack<Integer> second = new Stack<>();
+
+	public void insert(int element) {
+		stack.push(element);
+	}
+		
+	//Unlike stack, this should remove the head, bottom element of the stack
+	public int remove() {
+		if(stack.isEmpty() && second.isEmpty()) {
+			return -1; //Underflow
+		}
+
+		if(second.isEmpty()) {
+			while(!stack.isEmpty()) {
+				second.push(stack.pop());
+			}
+		}
+
+		return second.pop();
+	}
+
+	public boolean isEmpty() {
+		return stack.isEmpty() && second.isEmpty();
+	}
+	
+	//Instead of using a secondary stack, can we recursively find and remove the bottom element alone
+	public int removeRecursive() {
+		if(stack.isEmpty()) {
+			return -1; 
+		} else if(stack.size() == 1) { //bottom element
+			return stack.pop();
+		} else {
+			int current = stack.pop();
+			int ret = removeRecursive();
+			stack.push(current); //push the other elements again
+			return ret; //bottom  element will be returned
+		}
+	}		
+
 }
